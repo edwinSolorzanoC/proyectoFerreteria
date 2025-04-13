a80b0629aef223d8dba69ad7d93ce1ffa43f4fff
unknown <danielum99@gmail.com>
2025-04-13 10:15:29 -0600

    fetch reporte productos

--- ui-next/src/app/admin/estadisticas/analisis/page.tsx
+++ ui-next/src/app/admin/estadisticas/analisis/page.tsx
@@ -2,13 +2,40 @@
 "use client";
 
 import Header from "../../../components/Header";
+import { useEffect, useState } from 'react';
+
+interface ReporteProducto {
+    nombre_producto: string;
+    total_vendido: number;
+}
 
 export default function page(){
+    const [data, setData] = useState<ReporteProducto[]>([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch("http://localhost:5000/api/reportes/productos")
+            if (!res.ok) {
+                throw new Error("Error al obtener datos de análisis");
+            }
+            const data: ReporteProducto[] = await res.json();
+            setData(data);
+        }
+        fetchData()
+    }, [])    
+
     return(
 
         <>
         <Header/>
-        
+        {
+            data.map((producto) => (
+                <div key={producto.nombre_producto} className="bg-gray-800 p-4 rounded-lg mb-4">
+                    <h2 className="text-xl font-bold text-white">{producto.nombre_producto}</h2>
+                    <p className="text-gray-400">Total vendido: {producto.total_vendido}</p>
+                </div>
+            ))
+        }
         </>
 
     )
